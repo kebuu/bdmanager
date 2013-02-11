54692ce470bf1331ea120b596b24be0d3c07a083
ctardella <christophe.tardella@gmail.com>
2013-02-11 14:30:08 +0100

    removing target folder

--- target/test-classes/sql/test-serie.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Serie (id, name, mark, synopsis) values (-1, 'Le combat ordinaire', null, null);
-INSERT INTO Serie (id, name, mark, synopsis) values (-2, 'Le régulateur', null, null);
-INSERT INTO Serie (id, name, mark, synopsis) values (-3, 'Largo Winch', null, null);
-INSERT INTO Serie (id, name, mark, synopsis) values (-4, 'La trilogie Nikopol', null, null);
-INSERT INTO Serie (id, name, mark, synopsis) values (-5, 'Le sommeil du monstre', null, null);
