cfe1670907c9cd4037db3628e29e04257aa6a775
ctardella <christophe.tardella@gmail.com>
2013-04-17 21:37:34 +0200

    First test spring batch OK

--- bdmanager-batch/src/main/java/com/cta/launcher/TestBatch.java
+++ bdmanager-batch/src/main/java/com/cta/launcher/TestBatch.java
@@ -1,6 +1,7 @@
 package com.cta.launcher;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
@@ -15,6 +16,6 @@ public class TestBatch {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring/batch-context.xml");
         JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher", JobLauncher.class);
         Job job = (Job) context.getBean("helloWorldJob", Job.class);
-        jobLauncher.run(job, null);
+        jobLauncher.run(job, new JobParameters());
     }
 }
